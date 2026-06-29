e731e8f23b70bbc8f8f8813eac6ff784c81cbb9c
zinsss <87020914+zinsss@users.noreply.github.com>
2026-06-29 22:45:34 +0900

    Add PACS auto polling timer

--- KaosEghis/config.py
+++ KaosEghis/config.py
@@ -11,6 +11,8 @@ class AppConfig:
     eghis_db_image_study_query: str = ""
     kaospacs_api_base_url: str = "http://127.0.0.1:8055"
     kaospacs_api_timeout_seconds: str = "5"
+    pacs_auto_poll_enabled: str = "false"
+    pacs_poll_interval_seconds: str = "60"
 
 
 DEFAULT_CONFIG = AppConfig()
